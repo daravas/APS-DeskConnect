9fa26996578a7651a1f2f8ec6efe23676f3db8b2
Douglas Ventura <douglasventura.pe@gmail.com>
2023-12-13 07:21:00 -0300

    add project structure

---
+++ code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/DeskconnectApplication.java
@@ -0,0 +1,13 @@
+package com.grupo11.aps.deskconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeskconnectApplication.class, args);
+	}
+
+}
---
+++ code/deskconnect/src/test/java/com/grupo11/aps/deskconnect/DeskconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo11.aps.deskconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
