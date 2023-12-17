9a1d77896b95c42ac4cd8ff0b5ac4cf3e3fe9fd3
Anna Luiza Caraciolo <alcaf@cin.ufpe.br>
2023-12-17 16:05:15 -0300

    Adicionando banco de dados

--- code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/dados/ContaDAO.java
+++ code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/dados/ContaDAO.java
@@ -1,8 +1,8 @@
 package com.grupo11.aps.deskconnect.dados;
 
 import com.grupo11.aps.deskconnect.negocio.Conta;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ContaDAO extends CrudRepository<Conta, Long> {
+public interface ContaDAO extends JpaRepository<Conta, Long> {
 }
--- code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/negocio/Conta.java
+++ code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/negocio/Conta.java
@@ -1,6 +1,11 @@
 package com.grupo11.aps.deskconnect.negocio;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Conta {
+    @Id
     private Long id;
     private String login;
     private String senha;
