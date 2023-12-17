fa83de299b272dd5024f2ec1475384799e7c90de
Douglas Ventura <douglasventura.pe@gmail.com>
2023-12-17 13:35:40 -0300

    add exceptions for database and resource not found

---
+++ code/deskconnect/src/main/java/com/grupo11/aps/deskconnect/negocio/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.grupo11.aps.deskconnect.negocio.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id: " + id);
+    }
+}
